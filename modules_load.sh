08aa2de75a295631e11da6f7e4d515cec91213de
ozdalkiran-l <yacob.ozdalkiran@gmail.com>
2026-02-18 17:58:47 +0100

    Added slurm scripts

---
+++ modules_load.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+module purge
+module load intel-oneapi-compilers/2025.3.1/none-none
+module load intel-oneapi-mpi/2021.17.1/intel-oneapi-compilers-2025.3.1
+module load cmake/3.31.9/gcc-15.1.0
