080c40dbbba30aa9580df9a9b6af384c8f25718e
ozdalkiran-l <yacob.ozdalkiran@gmail.com>
2026-02-19 17:24:11 +0100

    test scripts

---
+++ testhb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=hb_test
+#SBATCH --output=%x.o
+#SBATCH --time=00:20:00
+#SBATCH --ntasks=16
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --partition=cpu_short
+
+#Source necessary modules
+source modules_load.sh
+
+# Run MPI script
+srun build/gauge_hb_cb inputs/hb_cb.txt
